8a82a70fea90db506f1928aaeb571c8b2811a698
Finnur Bragason <fbragason@outlook.com>
2021-07-14 13:52:16 +0800

    .

--- src/CombinedFileRequest.cs
+++ src/CombinedFileRequest.cs
@@ -1,5 +1,6 @@
 ﻿using Pidgin;
 using Pidgin.Comment;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using static Pidgin.Parser;
@@ -13,6 +14,8 @@ namespace Celin.AIS.Data
         => Map((o, s, q) => new DatabrowserRequest()
         {
             batchDataRequest = true,
+            formServiceDemo = o.HasValue && o.Value.Item1.HasValue ? Request.TRUE : null,
+            maxPageSize = o.HasValue && o.Value.Item3.HasValue ? o.Value.Item3.Value : null,
             dataRequests = s.Select(r =>
             {
                 r.query = q.Count() > 0
@@ -22,37 +25,43 @@ namespace Celin.AIS.Data
                         condition = q.Count() == 1
                             ? q.First().Item3.Select(c => new Condition
                             {
-                                controlId = $"{r.targetName}.{c.controlId}",
+                                controlId = c.controlId.Contains('.')
+                                    ? c.controlId.Split('.')[0].Equals(r.targetName)
+                                        ? c.controlId
+                                        : null
+                                    : $"{r.targetName}.{c.controlId}",
                                 @operator = c.@operator,
                                 value = c.value
-                            })
+                            }).Where(c => c.controlId != null)
                             : null,
                         complexQuery = q.Count() > 1
                             ? q.Select(cq =>
+                            {
+                                var qry = new ComplexQuery
                                 {
-                                    var qry = new ComplexQuery
+                                    andOr = last.ToString("G"),
+                                    query = new Query()
                                     {
-                                        andOr = last.ToString("G"),
-                                        query = new Query()
+                                        matchType = cq.Item2.ToString("G"),
+                                        condition = cq.Item3
+                                        .Select(c => new Condition
                                         {
-                                            matchType = cq.Item2.ToString("G"),
-                                            condition = (cq.Item3 as List<Condition>)
-                                            .Select(c => new Condition
-                                            {
-                                                @operator = c.@operator,
-                                                controlId = $"{r.targetName}.{c.controlId}",
-                                                value = c.value
-                                            })
-                                        }
-                                    };
-                                    last = cq.Item1.HasValue ? cq.Item1.Value : AndOrCombinator.AND;
-                                    return qry;
-                                })
+                                            controlId = c.controlId.Contains('.')
+                                                ? c.controlId.Split('.')[0].Equals(r.targetName)
+                                                    ? c.controlId
+                                                    : null
+                                                : $"{r.targetName}.{c.controlId}",
+                                            @operator = c.@operator,
+                                            value = c.value
+                                        }).Where(c => c.controlId != null)
+                                    }
+                                };
+                                last = cq.Item1.HasValue ? cq.Item1.Value : AndOrCombinator.AND;
+                                return qry;
+                            })
                             : null
                     }
                     : null;
-                r.formServiceDemo = o.HasValue && o.Value.Item1.HasValue ? "TRUE" : null;
-                r.maxPageSize = o.HasValue && o.Value.Item3.HasValue ? o.Value.Item3.Value : null;
                 return r;
             }),
         },
--- src/DataRequest.cs
+++ src/DataRequest.cs
@@ -20,28 +20,44 @@ namespace Celin.AIS.Data
             aggregation = a.HasValue ? a.Value.Aggregation : null,
             findOnEntry = Request.TRUE,
             query = q.Count() > 0
-                        ? new Query()
-                        {
-                            matchType = q.Count() == 1 ? q.First().Item2.ToString("G") : null,
-                            condition = q.Count() == 1 ? q.First().Item3 : null,
-                            complexQuery = q.Count() > 1
-                                ? new List<ComplexQuery>(q.Select(r =>
+                ? new Query()
+                {
+                    matchType = q.Count() == 1 ? q.First().Item2.ToString("G") : null,
+                    condition = q.Count() == 1
+                    ? q.First().Item3.Select(c => new Condition
+                    {
+                        controlId = c.controlId.Contains('.')
+                        ? c.controlId
+                        : $"{s.Name.ToUpper()}.{c.controlId}",
+                        @operator = c.@operator,
+                        value = c.value
+                    })
+                    : null,
+                    complexQuery = q.Count() > 1
+                        ? q.Select(r =>
+                            {
+                                var qry = new ComplexQuery
+                                {
+                                    andOr = last.ToString("G"),
+                                    query = new Query()
                                     {
-                                        var qry = new ComplexQuery
+                                        matchType = r.Item2.ToString("G"),
+                                        condition = r.Item3.Select(c => new Condition
                                         {
-                                            andOr = last.ToString("G"),
-                                            query = new Query()
-                                            {
-                                                matchType = r.Item2.ToString("G"),
-                                                condition = r.Item3 as List<Condition>
-                                            }
-                                        };
-                                        last = r.Item1.HasValue ? r.Item1.Value : AndOrCombinator.AND;
-                                        return qry;
-                                    }))
-                                : null
-                        }
-                        : null,
+                                            controlId = c.controlId.Contains('.')
+                                            ? c.controlId
+                                            : $"{s.Name.ToUpper()}.{c.controlId}",
+                                            @operator = c.@operator,
+                                            value = c.value
+                                        })
+                                    }
+                                };
+                                last = r.Item1.HasValue ? r.Item1.Value : AndOrCombinator.AND;
+                                return qry;
+                            })
+                        : null
+                }
+                : null,
             formServiceDemo = o.HasValue && o.Value.Item1.HasValue ? Request.TRUE : null,
             outputType = o.HasValue && o.Value.Item2.HasValue ? Request.VERSION2 : Request.GRID_DATA,
             maxPageSize = o.HasValue && o.Value.Item3.HasValue
