6059b6681f472e092e718bc62dbab5f674ee0ad7
Finnur Bragason <fbragason@outlook.com>
2023-07-16 12:13:56 +0800

    beta6

---
+++ Tok.cs
@@ -0,0 +1,13 @@
+﻿using Pidgin;
+using static Pidgin.Parser;
+
+namespace Celin.AIS.Data;
+
+public class Base
+{
+    public static Parser<char, T> Tok<T>(Parser<char, T> p)
+        => Try(p).Before(SkipWhitespaces);
+
+    public static Parser<char, char> Tok(char value) => Tok(Char(value));
+    public static Parser<char, string> Tok(string value) => Tok(String(value));
+}
